eaf3f486e689ff78aec9cf21272f5b86e36301b8
JordanB <jordan.bigre@croisix.com>
2025-05-21 14:29:57 +0200

    Update CI/CD configuration for awsdemo project

--- deploy-cicd.sh
+++ deploy-cicd.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Paramètres
-REPO="ton-utilisateur/immoloc-front"
-BRANCH="main"
+REPO="JordanB001/aws-test"
+BRANCH="master"
 TOKEN="ghp_xxxxxxxx"
-BUCKET="aws.jordanb.ovh"
+BUCKET="awsdemo.jordanb.ovh"
 
 # 1. Crée les rôles IAM
 aws cloudformation deploy \
   --template-file cfn-iam-roles.yaml \
-  --stack-name immoloc-iam-roles \
+  --stack-name awsdemo-iam-roles \
   --capabilities CAPABILITY_NAMED_IAM
 
 # 2. Récupère les ARNs générés
 CB_ROLE=$(aws cloudformation describe-stacks \
-  --stack-name immoloc-iam-roles \
+  --stack-name awsdemo-iam-roles \
   --query "Stacks[0].Outputs[?OutputKey=='CodeBuildRoleArn'].OutputValue" --output text)
 
 CP_ROLE=$(aws cloudformation describe-stacks \
-  --stack-name immoloc-iam-roles \
+  --stack-name awsdemo-iam-roles \
   --query "Stacks[0].Outputs[?OutputKey=='CodePipelineRoleArn'].OutputValue" --output text)
 
 # 3. Crée la pipeline CI/CD
 aws cloudformation deploy \
-  --template-file cfn-ci-cd.yaml \
-  --stack-name immoloc-front-cicd \
+  --template-file cfn-ci-cd.yml \
+  --stack-name awsdemo-front-cicd \
   --parameter-overrides \
     GitHubRepo=$REPO \
     GitHubBranch=$BRANCH \
